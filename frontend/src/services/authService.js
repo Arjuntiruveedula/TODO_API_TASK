b29d6d802e23df602f1a5ffe028924ac1ce87255
Apparao Tiruveedula <131840796+Arjuntiruveedula@users.noreply.github.com>
2025-03-17 09:26:54 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://todo-api-task-crk3.vercel.app/api/auth';
 
 const register = async (userData) => {
   
