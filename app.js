7af76aecced653127d8c1e89d4eb586dff8cb60b
simhadhri tiruveedhula <roapp4455@gmail.com>
2025-03-17 07:03:39 +0530

    first commit

--- app.js
+++ app.js
@@ -15,17 +15,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 // Routes
+app.get('/', (req, res) => {
+  res.send('TODO LIST API');
+  });
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/todos', require('./routes/todoRoutes'));
+
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
-  });
-}
+
 
 // Error handling middleware
 app.use(errorHandler);
