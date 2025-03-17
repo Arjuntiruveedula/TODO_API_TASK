9dc4d678f94471d5f44080a2a1896d0b7f4e3897
Apparao Tiruveedula <131840796+Arjuntiruveedula@users.noreply.github.com>
2025-03-17 09:18:47 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getUsers, getUserById, deleteUser } = require('../controllers/userController');
 const { protect } = require('../middleware/auth');
 
-router.get('/', protect, getUsers);
+router.get('/',  getUsers);
 router.get('/:id', protect, getUserById);
 router.delete('/:id', protect, deleteUser);
 
